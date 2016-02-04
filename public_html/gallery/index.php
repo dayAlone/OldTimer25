6a85790168e62df516138362d0b5259c36014259
dayAlone <andrey.slider@gmail.com>
2016-02-04 16:46:48 +0500

    Texts

--- public_html/gallery/index.php
+++ public_html/gallery/index.php
@@ -1,7 +1,7 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetPageProperty('body_class', "gallery");
-$APPLICATION->SetTitle('Олдтаймер-галерея');
+$APPLICATION->SetTitle('Олдтаймер-Галерея');
 $srt = urlencode("Билет на Олдтаймер-Гаререю и экспозицию ГОН");
 ?>
 <div class="header">
