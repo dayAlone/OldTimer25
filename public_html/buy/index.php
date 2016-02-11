1843c31f8d795fcd16c5f6bf01071d3ab1a5213d
dayAlone <andrey.slider@gmail.com>
2016-02-11 23:38:07 +0500

    Buy

--- public_html/buy/index.php
+++ public_html/buy/index.php
@@ -14,7 +14,7 @@ $srt = urlencode($srt);
 <div class="page">
   <div class="container">
     <div class="center">
-        <iframe style='display: inline-block;' frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/shop.xml?account=41001232198506&quickpay=shop&payment-type-choice=on&writer=seller&targets=<?=$srt?>&default-sum=500&button-text=02&mail=on&successURL=www.oldtimer.ru/oldtimer-gallery/visitors/&<?=(isset($_GET['promo'])?'comment=on':'')?>" width="450" height="235"></iframe>
+        <iframe style='display: inline-block;' frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/shop.xml?account=41001232198506&quickpay=shop&payment-type-choice=on&writer=seller&targets=<?=$srt?>&default-sum=500&button-text=02&mail=on&successURL=www.oldtimer.ru/oldtimer-gallery/visitors/&<?=(isset($_GET['promo'])?'comment=on':'')?>" width="450" height="<?=(isset($_GET['promo'])?'295':'235')?>"></iframe>
       <p> <strong>Онлайн продажа билетов со скидкой прекращается 4 марта в 12:00.<br/><br/></strong>Для покупки нескольких билетов, исправьте сумму на любую, кратную 500 руб.<br/>
         Электронные билеты рассылаются в автоматическом режиме в течение 15 минут после оплаты.<br/>
         Если оплаченный билет не получен Вами в течение часа, напишите нам на <a href="mailto:gallery@oldtimer.ru" target="_blank">gallery@oldtimer.ru</a><br/><br/>
