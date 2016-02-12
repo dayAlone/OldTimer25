f3a94a50c2e35a2c58df35f26400c2334556436b
dayAlone <andrey.slider@gmail.com>
2016-02-12 17:06:10 +0500

    Promo

--- public_html/promo/index.php
+++ public_html/promo/index.php
@@ -3,8 +3,9 @@ require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetPageProperty('body_class', "buy");
 $APPLICATION->SetTitle('Билет онлайн');
 $srt = "Билет на «Олдтаймер-Галерею» и экспозицию «95 лет ГОН»";
-$cost = 500;
-if (intval($_GET['card']) > 0) $srt .= " + Открытка ".$_GET['card'];
+$srt .= " + Промобилет";
+$cost = 1000;
+
 
 $srt = urlencode($srt);
 ?>
@@ -19,11 +20,13 @@ $srt = urlencode($srt);
             frameborder="0"
             allowtransparency="true"
             scrolling="no"
-            src="https://money.yandex.ru/embed/shop.xml?account=41001232198506&quickpay=shop&payment-type-choice=on&writer=seller&targets=<?=$srt?>&default-sum=<?=$cost?>&button-text=02&mail=on&successURL=www.oldtimer.ru/oldtimer-gallery/visitors/&<?=(isset($_GET['promo'])?'comment=on':'')?>" width="450" height="<?=(isset($_GET['promo'])?'295':'235')?>">
+            src="https://money.yandex.ru/embed/shop.xml?account=41001232198506&quickpay=shop&payment-type-choice=on&writer=seller&targets=<?=$srt?>&default-sum=<?=$cost?>&button-text=02&mail=on&successURL=www.oldtimer.ru/oldtimer-gallery/visitors/&comment=on" width="450" height="295">
         </iframe>
       <p>
-        <strong>Онлайн продажа билетов со скидкой прекращается 4 марта в 12:00.<br/><br/></strong>
-        Для покупки нескольких билетов, исправьте сумму на любую, кратную 500 руб.<br/>
+        Для участия в акции "Каждый третий – бесплатно!" впишите промокод в поле "Комментарий" и оплатите два билета на выставку по 500 рублей каждый. Третий билет вы получите бесплатно.
+        Чтобы купить большее количество билетов, исправьте сумму на любую, кратную 1000 рублей.
+        Акция закончится не позднее 25 февраля. Количество билетов, участвующих в акции, ограничено. По окончании акции эта страница станет недоступной.
+
 
         Электронные билеты рассылаются в автоматическом режиме в течение 15 минут после оплаты.<br/>
         Если оплаченный билет не получен Вами в течение часа, напишите нам на <a href="mailto:gallery@oldtimer.ru" target="_blank">gallery@oldtimer.ru</a><br/><br/>
